86e2bf7fb020a4a4bb282e187e5b204617198feb
Bojankozina <bojankozina@outlook.com>
2025-11-25 20:17:58 +0100

    Deleted everything

--- Market.Backend/Market.Application/Modules/Countries/Queries/List/ListCountriesQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-using System.Collections.Generic;
-
-namespace Market.Application.Modules.Countries.Queries.List
-{
-    public sealed class ListCountriesQuery : BasePagedQuery<ListCountriesQueryDto>
-    {
-    }
-}
--- Market.Backend/Market.Application/Modules/Countries/Queries/List/ListCountriesQueryDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Market.Application.Modules.Countries.Queries.List;
-
-public sealed class ListCountriesQueryDto
-{
-    public int Id { get; init; }
-    public string Name { get; init; } = default!;
-}   
