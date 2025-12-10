750997f8a2c744fbb06e6fbf27ac4a6a7817bb4d
Malikpeco <malikpeco@gmail.com>
2025-12-10 23:23:14 +0100

    Created CreateAddToCartCommand
    Added AddToCart command

---
+++ Market.Backend/Market.Application/Modules/AddToCart/Commands/AddToCartCommand.cs
@@ -0,0 +1,14 @@
+﻿using Market.Application.Modules.RegisterUser.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Application.Modules.AddToCart.Commands
+{
+    public sealed class AddToCartCommand : IRequest<Unit>
+    {
+        required public int Id { get; set; }
+    }
+}
---
+++ Market.Backend/Market.Application/Modules/AddToCart/Commands/AddToCartCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Application.Modules.AddToCart.Commands
+{
+    public sealed class AddToCartCommandValidator : AbstractValidator<AddToCartCommand>
+    {
+
+        public AddToCartCommandValidator()
+        {
+
+            RuleFor(x => x.Id).NotEmpty().WithMessage("GameId is required.");
+        }
+    }
+}
