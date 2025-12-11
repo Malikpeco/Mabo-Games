a8650eb78b58ccff77be1c39154fcc0a9f9c538f
Malikpeco <malikpeco@gmail.com>
2025-12-11 23:42:59 +0100

    Reorganized file structure(moved AddToCart from seperate folder into the Carts->Commands->Create folder), and set up folders/files to implement Dtos,GetCartByUseerQuery and its handler

--- Market.Backend/Market.Application/Modules/Carts/Commands/Create/AddToCartCommandValidator.cs
+++ Market.Backend/Market.Application/Modules/Carts/Commands/Create/AddToCartCommandValidator.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Market.Application.Modules.AddToCart.Commands
+namespace Market.Application.Modules.Carts.Commands
 {
     public sealed class AddToCartCommandValidator : AbstractValidator<AddToCartCommand>
     {
---
+++ Market.Backend/Market.Application/Modules/Carts/Queries/GetCartByUserIdQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Market.Application.Modules.Carts.Queries
+//{
+//    public sealed class GetCartByUserIdQueryHandler (IAppDbContext context, IAppCurrentUser currentUser) 
+//        : IRequestHandler<GetCartByUserIdQuery, Unit>
+//    {
+
+//        public async Task<Unit> Handle(GetCartByUserIdQuery request, CancellationToken ct)
+//        {
+//            var c = await context.Carts
+//                .Where(x => x.UserId == currentUser.UserId)
+//                .FirstOrDefaultAsync(ct);
+
+//            return c;
+
+//        }
+//    }
+//}
