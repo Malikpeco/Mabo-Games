ac58d0463a951b294babd6f00c5cf96a4631eaaa
Malikpeco <malikpeco@gmail.com>
2025-12-13 21:37:51 +0100

    Completed AddToCart feature

---
+++ Market.Backend/Market.API/Controllers/CartController.cs
@@ -0,0 +1,19 @@
+﻿using Market.Application.Modules.Carts.Commands.Create;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Market.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class CartController(ISender sender) : ControllerBase
+    {
+        [HttpPost("AddToCart")]
+        public async Task<ActionResult> Add(AddToCartCommand command, CancellationToken ct)
+        {
+            await sender.Send(command, ct);
+            return Ok();
+        }
+    }
+}
