f78f4995dab24b698c84a172571fad4f4cde15e9
Malikpeco <malikpeco@gmail.com>
2025-12-30 21:57:32 +0100

    Added CRUD endpoints for Game entity

---
+++ Market.Backend/Market.Application/Modules/Games/Commands/Create/CreateGameCommand.cs
@@ -0,0 +1,19 @@
+﻿using Market.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Application.Modules.Games.Commands.Create
+{
+    public sealed class CreateGameCommand : IRequest<int>//when this command is handled it returns an int (gameId)
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int PublisherId { get; set; }
+        public string? CoverImageURL { get; set; }
+    }
+}
