567c8bce41d946e292fe5fc678077e9731e37a99
Malikpeco <malikpeco@gmail.com>
2026-01-02 21:00:21 +0100

    added: add favourite(user), remove favourite(user), get favourites(user)

---
+++ Market.Backend/Market.Application/Modules/Favourites/Queries/List/ListFavouritesQuery.cs
@@ -0,0 +1,13 @@
+﻿using Market.Application.Modules.Games.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Application.Modules.Favourites.Queries.List
+{
+    public sealed class ListFavouritesQuery : BasePagedQuery<StorefrontGameDto>
+    {
+    }
+}
